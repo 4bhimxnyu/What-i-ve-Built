0d01aed975ea1737f9bfc3ce45743cbc46a63716
4bhimxnyu <abhimanyupratapsingh30nov1106@gmail.com>
2026-06-04 17:04:08 +0530

    authentication with bcrypt

--- Login System/index.js
+++ Login System/index.js
@@ -8,15 +8,14 @@ app.use(express.json());
 
 app.post("/login", (req, res) => {
 
-    const { username, password } = req.headers;
+    const { username, password } = req.body;
 
     if (
         username === "admin" &&
         password === "123"
     ) {
 
-        return res.json({
-           
+        return res.json({  
             message: "Login successful"
         })
         ;
@@ -28,6 +27,43 @@ app.post("/login", (req, res) => {
     });
 
 });
+
+
+let users = [];
+
+app.post("/register",(req,res) =>{
+    const { username, password } = req.body;
+    users.push({
+        username,
+        password
+    });
+
+    res.json({
+        message: "User registered successfully"
+    });
+})
+
+//checking all the users using get request
+app.get("/dataprofile",(req,res) =>{
+    res.json(users);
+});
+
+app.post("/sum",(req,res) =>{
+    const { num1, num2 } = req.body;
+    console.log(num1,num2);
+    const sum = num1 + num2;
+    console.log(sum);
+    res.json({
+        sum
+    });
+})
+
+
+app.get("/hello",(req,res) =>{
+    res.json({
+        message: "Hello World"
+    })
+})
 
 app.listen(3000);
 
---
+++ Login System/real.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+app.use(express.json());
+
+const PORT = 3000;
+
+let users = [];
+
+app.get("/", (req,res) =>{
+    res.json({
+        message: "Welcome to the login system"
+    });
+    console.log("this is the home page");
+})
+
+app.post("/register", (req, res) =>{
+
+    const {username, password} = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    users.push({
+        username,
+        password : hashedPassword
+    })
+
+    
+    console.log(users);
+    res.json({
+        message: "User has been registered successfully"
+    })
+}); 
+
+app.get("/users", (req, res) =>{
+    res.json(users);
+});
+
+app.get("/users/:username", (req,res) =>{
+    const username = req.params.username;
+    console.log(username);
+
+    const user = users.find(
+        u => u.username === username 
+    );
+
+    if(!user){
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+    console.log(`user has been found ${user.username}`);
+    res.status(200).json({user})
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port https://localhost:3000`);
+})
