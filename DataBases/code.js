8c1f39b913986aca9b1271129999725296fa69c0
4bhimxnyu <abhimanyupratapsingh30nov1106@gmail.com>
2026-06-12 20:52:30 +0530

    mongodb connections success

--- DataBases/code.js
+++ DataBases/code.js
@@ -2,24 +2,61 @@ require('dotenv').config({ path: '../.env' });
 
 const express = require('express');
 const mongoose = require('mongoose');
-
+const jwt = require("jsonwebtoken")
+const Secret_key = process.env.SECRET_KEY;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log(' MongoDB Atlas connected');
+ const User = mongoose.model('Users', { name : String, email : String ,password: String });
 
-    app.listen(3000, () => {
-      console.log(' Server running on port 3000');
-    });
-  })
-  .catch((err) => {
-    console.error(' MongoDB connection failed');
-    console.error(err);
-  });
+// const auth = (req,res,next) => {
+//     const {username , password } = req.body;
+//     const name = req.body.name ;
+    
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//     const ExistingUser = username.f
+//     if(!(username || password)){
+//         return res.status(400).json({
+//             message: "username or password missing."
+//         })
+//     }
+// }
+
+
+app.get("/", (req,res) =>{
+    res.json({
+        message : "Welcome to the homepage"
+    })
+})
+
+app.post("/signup", async (req,res) =>{
+    const {username , password} = req.body;
+    const name = req.body.name;
+ 
+    const ExistingUser = await User.findOne({ email : username});
+    if(ExistingUser){
+        res.status(400).json({
+            message : "user already exists"
+        })
+    }
+
+    const newPassword = await bcrypt.hash(password, 10);
+
+    const User = new User({
+        name: name,
+        email: username,
+        password : newPassword
+    })
+
+
+    user.save();
+    res.json({
+        message : "user registered successfully"
+    })
+})
+
+app.listen(PORT ,() => {
+    console.log(`your code is running on https://localhost${PORT}`)}
+)
